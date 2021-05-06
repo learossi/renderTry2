37d51b40ad16480bb72f515ba4760c550694aa57
learossi <78810859+learossi@users.noreply.github.com>
2021-05-05 21:15:35 -0500

    Update Light.h

--- renderTry2/Light.h
+++ renderTry2/Light.h
@@ -9,13 +9,15 @@
 
 class Light {//use as a parent class if I implement more than one light type
 public:	
-	vec3 d; //light direction
+	vec3 d; //light direction or origin
 	float i; //light intensity
 	float r;
 	float g;
 	float b;
 	virtual bool shadowCheck(ray& r, vec3& normal, shapeList& shapeRecord);
+	virtual vec3 lightDirectionReturn(vec3 intersectionPoint);
 };
 
 bool Light::shadowCheck(ray& r, vec3& normal, shapeList& shapeRecord) { return true; };
+vec3 Light::lightDirectionReturn(vec3 intersectionPoint) { return vec3(); };
 #endif // !Light_h
